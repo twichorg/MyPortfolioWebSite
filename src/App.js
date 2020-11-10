5026c591f60837b826413bab50a096f7f624c098
can aktürk <52425556+twichorg@users.noreply.github.com>
2020-11-10 03:05:17 +0300

    message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MyNavbar from './components/my-navbar/mynavbar.component';
+
+const App = () => {
+  return (
+    <div className="App">
+     <MyNavbar/>
+     Portfolio
+    </div>
+  );
+}
+
+export default App;
